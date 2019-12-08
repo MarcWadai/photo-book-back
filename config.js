6f36f3e0381b8ecff525675b3415ea523b2f8f64
marc <marc.sirisak@goodeed.com>
2019-12-09 00:17:39 +0100

    correct config

--- config.js
+++ config.js
@@ -7,11 +7,11 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 module.exports = {
-    iam_access_id: keys.iam_access_id || process.env.IAM_ACCESS,
-    iam_secret: keys.iam_secret || process.env.IAM_SECRET,
-    dbUrl: keys.dbUrl || process.env.DB_URL,
-    dbUser: keys.dbUser || process.env.DB_USER,
-    dbPassword: keys.dbPassword || process.env.DB_PASSWORD,
-    authUser: keys.authUser || process.env.AUTH_USER,
-    authPassword: keys.authPassword || process.env.AUTH_PASWORD
+    iam_access_id: (process.env.NODE_ENV !== 'production') ? keys.iam_access_id : process.env.IAM_ACCESS,
+    iam_secret: (process.env.NODE_ENV !== 'production') ?keys.iam_secret || process.env.IAM_SECRET,
+    dbUrl: (process.env.NODE_ENV !== 'production') ? keys.dbUrl : process.env.DB_URL,
+    dbUser: (process.env.NODE_ENV !== 'production') ? keys.dbUser : process.env.DB_USER,
+    dbPassword: (process.env.NODE_ENV !== 'production') ? keys.dbPassword : process.env.DB_PASSWORD,
+    authUser: (process.env.NODE_ENV !== 'production') ? keys.authUser : process.env.AUTH_USER,
+    authPassword: (process.env.NODE_ENV !== 'production') ? keys.authPassword : process.env.AUTH_PASWORD
 }; 
